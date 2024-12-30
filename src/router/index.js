97a89aafa27ac69333493c3f22472aa9d9f85983
bitdan <abiasnip@gmail.com>
2024-12-30 19:53:29 +0800

    feat(router): 更新路由配置并重命名页面标题

--- src/router/index.js
+++ src/router/index.js
@@ -7,21 +7,25 @@ import TimestampTool from '../components/TimestampTool.vue'
 const routes = [
     {
         path: '/',
+        redirect: '/case-converter'
+    },
+    {
+        path: '/home',
         name: 'Home',
         component: Home,
-        meta: { title: '首页' }  // 使用 meta 存储显示名称
+        meta: { title: '首页' }
     },
     {
         path: '/case-converter',
         name: 'CaseConverter',
         component: CaseConverter,
-        meta: { title: '大小写转换' }  // 使用 meta 存储显示名称
+        meta: { title: '驼峰转换' }  
     },
     {
         path: '/string-generator',
         name: 'StringGenerator',
         component: StringGenerator,
-        meta: { title: '字符串生成' }  // 使用 meta 存储显示名称
+        meta: { title: '字符串生成' }  
     },
     {
         path: '/timestamp',
